cf555bc659d71c7836b46dd7fca2a759e17a3946
Hardik <hardikvagrecha@gmail.com>
2024-04-23 23:59:28 -0700

    route.ts for kindeAuth updated

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,12 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(request: Request, { params }: any) {
-  const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint);
+interface GETParams {
+  params: {
+    [key: string]: string;
+  };
+}
+
+export async function GET(request: any, { params }: GETParams): Promise<any> {
+  const endpoint = params?.kindeAuth;
+  return handleAuth(request, endpoint);
 }
